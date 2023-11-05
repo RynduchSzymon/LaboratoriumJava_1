1b20273c121b757bc26c18bbd5f2576c0e42cf8b
RynduchSzymon <szymon_rynduch@outlook.com>
2023-11-05 13:26:36 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+//ZADANIE 2 LABOLATORIUM NR.1
+//class Hello{
+//    public static void main(String[] args){
+//        System.out.println("Hello World");
+//    }
+//}
+
+//ZADANIE 3
+
+//class Student{
+//    public static void main(String[] args){
+//        System.out.println("Imie: Adam");
+//        System.out.println("Nazwisko: Kowalski");
+//    }
+//}
+
+//ZADANIE 4
+
+//import java.util.Scanner;
+//class InformacjeOStudencie{
+//    public static void main(String[] args){
+//        Scanner informacje = new Scanner(System.in);
+//        System.out.println("Czesc zaptam cie o twoje dane!");
+//        System.out.println("Podaj imie:");
+//        String ImieStudenta = informacje.nextLine();
+//        System.out.println("Podaj nazwisko:");
+//        String NazwiskoStudenta = informacje.nextLine();
+//        System.out.println("Podaj Kraj pochodzenia :");
+//        String KrajPochodzeniaStudenta = informacje.nextLine();
+//
+//
+//        System.out.println("Podsumowanie: ");
+//        System.out.println("Imie: "+ ImieStudenta + "\n" + "nazwisko: " + NazwiskoStudenta + "\n"+ "Kraj pochodzenia: " + KrajPochodzeniaStudenta);
+//        informacje.close();
+//    }
+//}
